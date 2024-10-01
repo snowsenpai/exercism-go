6fdf9137b1e3b49d1a9db40c042d4bfbf5f21f28
Umoh Samuel <samuelwisdom360@gmail.com>
2024-10-01 07:59:40 +0100

    complete cars assemble

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -3,16 +3,36 @@ package cars
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	successRatePercentage := successRate / float64(100)
+	workingCarsPerHour := float64(productionRate) * successRatePercentage
+
+	return workingCarsPerHour
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	workingCarsPerHour := CalculateWorkingCarsPerHour(productionRate, successRate)
+	minutesInOneHour := 60
+	workingCarsPerMinute := int(workingCarsPerHour) / minutesInOneHour
+
+	return workingCarsPerMinute
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	costPerCar := 10_000
+	costPerBatchOfTen := 95_000
+
+	carsPerGroup := 10
+
+	numFullBatches := carsCount / carsPerGroup
+	remainingCars := carsCount % carsPerGroup
+
+	totalBatchCost := numFullBatches * costPerBatchOfTen
+	totalRemainingCarsCost := remainingCars * costPerCar
+
+	totalCost := totalBatchCost + totalRemainingCarsCost
+
+	return uint(totalCost)
 }
