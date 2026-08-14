97e46ce4bd34bd08f147da6a2d619918083dcd92
Umoh Samuel <samuelwisdom360@gmail.com>
2026-08-14 15:30:59 +0100

    completed weather forecast exercise on commenting

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,14 @@
+// Package weather provides a forecasting functionality that describes the
+// current weather condition of a specific city.
 package weather
 
+// CurrentCondition represents the current weather condition of a city.
 var CurrentCondition string
+
+// CurrentLocation represents a city who's current weather is being forecasted.
 var CurrentLocation string
 
+// Forecast returns a string value that represents the current weather condition of a specific city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
